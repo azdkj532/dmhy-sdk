6bcc7e57eec1bec6821df4242ae1cb6472d28004
azdkj532 <kanade123321@gmail.com>
2014-12-26 17:19:34 +0800

    new api i have created the class 'dmhy' ,which includes some property of the topic, topics name, url, and magnet link.

--- dmhy/__init__.py
+++ dmhy/__init__.py
@@ -6,6 +6,63 @@ from urllib.parse   import quote
 
 from bs4 import BeautifulSoup
 
+class dmhy():
+    def __init__( self, title="", url="" ):
+        self._title = title
+        self._url = url
+        self._magnet = ""
+    def __str__( self ):
+        return self._title
+    def __rper__( self ):
+        return self._title
+
+    #parameter url must be an absolute path ( including http://... )
+    @staticmethod
+    def _GetMagnetLink( url ):
+        try:
+            res = urlopen( url )
+        except:
+            return None
+             #the http status
+        data = res.read().decode('utf-8')
+        if res.status != 200:
+            print( "network error: %d " % res.status )
+            return None
+        pattern = re.search( r"magnet:[^\"\s<>]*", data )
+        if pattern:
+            return pattern.group()
+        else:
+            return None
+
+    @property
+    def title(self):
+        return self._title
+    @title.setter
+    def title(self, title):
+        if not isinstance(title, str):
+            raise TypeError("Excepted a string")
+        self._title = title
+
+    @property
+    def magnet(self):
+        if not self._magnet:
+            self._magnet = self._GetMagnetLink(self._url)
+        return self._magnet
+    @magnet.setter
+    def magnet(self, magnet):
+        if not isinstance(magnet, str):
+            raise TypeError("Excepted a string")
+        self._magnet = magnet
+
+    @property
+    def url(self):
+        return self._url
+    @url.setter
+    def url(self, url):
+        if not isinstance(url, str):
+            raise TypeError("Excepted a string")
+        self._url = url
+
 #get an parameter, keywords it can be a string or a list
 def Search( keywords ):
 
@@ -13,50 +70,30 @@ def Search( keywords ):
         #keywords should split by space(s)
         keywords = filter( None ,keywords.split(' '))
     else:
-        return []
+        raise StopIteration
     keywords = [ quote(_) for _ in filter( None, keywords )]
     url = u"http://share.dmhy.org/topics/list?keyword={keyword}".format( keyword='+'.join(keywords) ) 
     
     try:
         res = urlopen(url)
     except:
-        return []
-        
+        raise StopIteration
+
     if res.status != 200 :
-        return []
+        raise StopIteration
 
     parser = BeautifulSoup( res.read() )
     try:
         table = parser.find( id='topic_list').tbody.find_all('tr')
-    except: return []
+    except: 
+        raise StopIteration
     else:
-        download_list = []
         for topic in table:
-            date = topic.find(style="display: none;").get_text()
+            #date = topic.find(style="display: none;").get_text()
             source = topic.find( target="_blank" )
             while source.find('span') != None :
                 source.span.unwrap()
-            title = re.findall( r"\S+.*", source.get_text() )
-            if len(title) > 0: 
-                title = title[0] #the first result that reg had matched is what we want
-            else: 
-                continue #if reg didn't match anything, pass this topic
-            download_list.append({ 'title':title, 'url':source['href'], 'date':date })
-        return download_list
+            title = source.get_text().strip()
+            url   = "http://share.dmhy.org"+source['href']
+            yield dmhy( title=title, url=url )
 
-#parameter url must be an absolute path ( including http://... )
-def GetMagnetLink( url ):
-    try:
-        res = urlopen( url )
-    except:
-        return None
-         #the http status
-    data = res.read().decode('utf-8')
-    if res.status != 200:
-        print( "network error: %d " % res.status )
-        return None
-    magnet = re.findall( r"magnet:[^\"\s<>]*", data )
-    if len(magnet) != 0:
-        return magnet[0]
-    else:
-        return None
