7bb64d523c15cadc80683e5066af2d58523adc02
Squirrel <kanade123321@gmail.com>
2016-01-15 15:30:16 +0800

    update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dmhy',
-      version='1.0',
+      version='1.1',
       description='a parser for dmhy',
       url='http://github.com/azdkj532/dmhy/',
       author='azdkj AZ',
